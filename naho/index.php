cf08d2f2e409ce7facbb087da8431b0d5840d454
naho0615 <cocoa03040615@gmail.com>
2017-07-01 13:32:28 +0900

    post get commit

---
+++ naho/index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+?>
+
+<html>
+<head>
+</head>
+<body>
+    <form action="result.php" method="post"> 
+        name:<input type="text" name="name" /><br>
+        pass:<input type="password" name="pass" /><br>
+        <input type="submit" value="登録" />
+    </form>
+</body>
+</html>
---
+++ naho/result.php
@@ -0,0 +1,12 @@
+<?php
+    echo $_POST['name'];
+    echo $_POST['pass'];
+?>
+
+<html>
+<head>
+</head>
+<body>
+    <h1>成功</h1>
+</body>
+</html>
